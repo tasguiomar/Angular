57df0f1f58275f487da4bf2aea3d301cd712e971
Unknown <tiagoalexandresanto@gmail.com>
2018-10-17 12:31:01 +0100

    mongoDb funciona

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 
    
 // connect moongose
-mongoose.connect('mongodb://localhost:8000/Angular',{
+mongoose.connect('mongodb://localhost:27017/',{
     useNewUrlParser: true
 });
 
